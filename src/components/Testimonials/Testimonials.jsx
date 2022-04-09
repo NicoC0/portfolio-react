533f55576c410ebc258764b19d95eb0b95e9d293
NicoC0 <nicoconfaa@gmail.com>
2022-04-09 03:22:03 -0300

    Testimonials component and css styles

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -3,7 +3,22 @@ import './testimonials.css'
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Reseñas de clientes</h5>
+      <h2>Valoraciones</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src="" alt="" />
+          </div>
+          <h5 className='client__name'>Nombre</h5>
+          <small className="client__review">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam laboriosam velit eligendi natus provident, accusamus sunt culpa officiis doloremque facere veritatis eum, repellat maiores illo adipisci at praesentium consequatur obcaecati.
+          </small>
+        </article>
+      </div>
+    </section>
   )
 }
 
