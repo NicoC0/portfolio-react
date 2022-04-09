37087abf06b3951073e94f7032be63df4b04955f
NicoC0 <nicoconfaa@gmail.com>
2022-04-09 07:39:59 -0300

    install 'emailjs' to be able to receive emails through the contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.css'
 import { MdOutlineMail } from 'react-icons/md'
 import { BsWhatsapp } from 'react-icons/bs'
 import { RiMessengerLine } from 'react-icons/ri'
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+  
+    emailjs.sendForm('service_oj10nbt', 'template_97x27z6', form.current, 'aXXLArOJpeacenvAU')
+    e.target.reset()
+  }
+
   return (
     <section id='contact'>
       <h5>Hablemos</h5>
@@ -34,11 +44,11 @@ const Contact = () => {
           </article>
         </div>
 
-          <form action="">
+          <form ref={form} onSubmit={sendEmail}>
             <input type='text' name='name' placeholder='Tu nombre completo' required />
             <input type='text' name='email' placeholder='Tu email' required />
             <textarea name='message' rows='7' placeholder='Tu mensaje' required></textarea>
-            <button type='submit' className='btn btn-primary'>Enviar mensaje</button>
+            <button type='submit' value='send' className='btn btn-primary'>Enviar mensaje</button>
           </form>
       </div>
     </section >
