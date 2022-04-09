7d294ca01f329f595a8f5011674ce0df2668cb3c
NicoC0 <nicoconfaa@gmail.com>
2022-04-09 08:22:37 -0300

    footer component and css style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,20 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <ul className='permalinks'>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#about">Sobre mi</a></li>
+        <li><a href="#experiencie">Mi experiencia</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Valoraciones</a></li>
+        <li><a href="#contact">Contacto</a></li>
+      </ul>
+
+      <div className="footer__copyright">
+        <small>&copy; Nicolás Confalonieri. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
