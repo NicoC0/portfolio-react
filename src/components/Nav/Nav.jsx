1d4bcbac692d7e6c124143d8af2e3a09cd4c2c21
NicoC0 <nicoconfaa@gmail.com>
2022-04-13 01:09:31 -0300

    Revert "new elements in nav"
    This reverts commit 6e87afb4abbf65ceb1abc655b9d1d4123a8bc020.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react'
 import './nav.css'
-import { AiOutlineHome, AiOutlineStar } from 'react-icons/ai'
+import { AiOutlineHome } from 'react-icons/ai'
 import { CgUserAdd } from 'react-icons/cg'
 import { BiBook } from 'react-icons/bi'
 import { SiMinutemailer } from 'react-icons/si'
-import { VscFolderOpened } from 'react-icons/vsc'
-
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
@@ -14,11 +12,11 @@ const Nav = () => {
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className = { activeNav === '#about' ? 'active' : ''}><CgUserAdd /></a>
       <a href="#experiencie" onClick={() => setActiveNav('#experiencie')} className = { activeNav === '#experiencie' ? 'active' : '' } ><BiBook /></a>
-      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className = { activeNav === '#portfolio' ? 'active' : '' } ><VscFolderOpened /></a>
-      <a href="#testimonials" onClick={() => setActiveNav('#testimonials')} className = { activeNav === '#testimonials' ? 'active' : '' } ><AiOutlineStar /></a>
       <a href="#contact" onClick = {() => setActiveNav('#contact')} className = { activeNav === '#contact' ? 'active' : ''}><SiMinutemailer /></a>
     </nav>
   )
 }
+
+
 
 export default Nav
